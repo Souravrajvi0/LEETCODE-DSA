3951855e0abe04004c1b87f32f1bd6931dc04b9b
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2023-09-23 11:22:32 +0530

    Time: 90 ms (76.67%) | Memory: 60.3 MB (6.98%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -7,9 +7,7 @@ public:
         int index=-1;
         while(low<=high){
           mid=low+(high-low)/2;
-          if(mid==arr.size()-1){
-              break;
-          }
+          
           if(arr[mid]<arr[mid+1]){
               low=mid+1;
           }else{
