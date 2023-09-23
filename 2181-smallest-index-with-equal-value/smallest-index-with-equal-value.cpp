f6e5b7bdf411feaf0ae13817f6f69f5719aefe5f
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2023-09-23 16:32:30 +0530

    Time: 13 ms (24.42%) | Memory: 22.2 MB (10.36%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.cpp
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
-            int t=i%10;
-            if(t==nums[i]){
+            if(nums[i]>=10){
+                continue;
+            }
+            if(nums[i]==i%10){
                 return i;
             }
         }
