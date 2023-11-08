f8ceec3b8f0c149878526b839a89cd6d4ff667cd
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2023-11-08 14:30:03 +0530

    Time: 3 ms (52.10%) | Memory: 7.4 MB (71.69%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
    
-   void gen(int i,int n,vector<int>& nums,vector<vector<int>>&res, vector<int>&cur){
-    if(i>=n){
+   void gen(int i,vector<int>& nums,vector<vector<int>>&res, vector<int>&cur){
+    if(i>=nums.size()){
       res.push_back(cur);
       return ;
       }
       cur.push_back(nums[i]);
-      gen(i+1,n,nums,res,cur);
+      gen(i+1,nums,res,cur);
       cur.pop_back();
-      gen(i+1,n,nums,res,cur);
+      gen(i+1,nums,res,cur);
       }
    
    
@@ -17,18 +17,12 @@ public:
    
    
    
-   
-   
-   
-   
-   
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int i=0;
-        int n=nums.size();
+      vector<vector<int>> subsets(vector<int>& nums) {
+     
+        
         vector<vector<int>>res;
         vector<int>cur;
-        gen(0,n,nums,res,cur);
-        
+        gen(0,nums,res,cur);
         return res;
     }
 };
