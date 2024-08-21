d0accd8328dddaa0f2bac58be320cdd182309792
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2024-08-21 13:48:20 +0530

    Time: 10 ms (15.30%) | Memory: 16.9 MB (9.06%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
 
     int findmin(vector<int>& cost,int i,vector<int>& dp){
-        if(i>=cost.size())return 0;
+        if(i==0)return cost[0];
+        if(i==1)return cost[1];
         if(dp[i]!=-1)return dp[i];
 
-        int sumofchoice1=cost[i]+findmin(cost,i+1,dp);
-        int sumofchoice2=cost[i]+findmin(cost,i+2,dp);
+        int sumofchoice1=cost[i]+findmin(cost,i-1,dp);
+        int sumofchoice2=cost[i]+findmin(cost,i-2,dp);
         dp[i]=min(sumofchoice1,sumofchoice2);
         return dp[i];
  }
 
 int minCostClimbingStairs(vector<int>& cost) {
         vector<int> dp(cost.size(),-1);
-        return min (findmin(cost,0,dp),findmin(cost,1,dp));
+        int n=cost.size();
+        return min (findmin(cost,n-1,dp),findmin(cost,n-2,dp));
         }
 };
