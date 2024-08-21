3635f71412ac1a23baef78eed126766280788709
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2024-08-21 19:42:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (33.28%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,18 +6,17 @@ public:
     if(dp[i]!=-1)return dp[i];
     
     return dp[i]= max(nums[i]+maxs(nums,i+2,dp),maxs(nums,i+1,dp));
-
-
-
-
-    }
-
-
-
-
+ }
     int rob(vector<int>& nums) {
-        vector <int> dp(nums.size(),-1);
-        return maxs(nums,0,dp);
+        if(nums.size()==1)return nums[0];
+        vector <int> dp(nums.size());
+        dp[nums.size()-1]=nums[nums.size()-1];
+        dp[nums.size()-2]=max(nums[nums.size()-1],nums[nums.size()-2]);
+
+        for(int i=nums.size()-3;i>=0;i--){
+            dp[i]=max(dp[i+1],nums[i]+dp[i+2]);
+        }
+        return dp[0];
         
     }
 };
