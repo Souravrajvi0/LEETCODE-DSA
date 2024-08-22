0133815102eb4bcbc3cf453ae5ab09b102ae7011
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2024-08-22 23:13:12 +0530

    Time: 4 ms (73.56%) | Memory: 11.7 MB (26.69%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>>dp;
     int help(vector<vector<int>>& triangle,int i,int j){
     if (i >= triangle.size()) return 0;
-    if(dp[i][j]!=-1)return dp[i][j];
+    if(dp[i][j]!=INT_MIN)return dp[i][j];
     
     int down=triangle[i][j]+help(triangle,i+1,j);
     int right= triangle[i][j]+help(triangle,i+1,j+1);
@@ -14,7 +14,7 @@ public:
  int minimumTotal(vector<vector<int>>& triangle) {
      dp.clear();
      int n = triangle.size();
-     dp.resize(n, vector<int>(n, -1));
+     dp.resize(n, vector<int>(n, INT_MIN));
      return help(triangle,0,0);
         
     }
