4a9da750a0e21378335db2a7562c28f91ed5d784
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2024-08-22 12:00:37 +0530

    Time: 4 ms (31.89%) | Memory: 9.4 MB (45.07%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    int maxs(vector<int>&nums,int i,vector <int>&dp){
-    if(i==nums.size()-1)return nums[i];
-    if(i==nums.size()-2)return max(nums[i],nums[i+1]);
-    if(dp[i]!=-1)return dp[i];
-    
-    return dp[i]= max(nums[i]+maxs(nums,i+2,dp),maxs(nums,i+1,dp));
- }
-    int rob(vector<int>& nums) {
-        if(nums.size()==1)return nums[0];
-        vector <int> dp(nums.size());
-        dp[nums.size()-1]=nums[nums.size()-1];
-        dp[nums.size()-2]=max(nums[nums.size()-1],nums[nums.size()-2]);
+  vector<int>dp;
+    int tp(vector<int>&nums,int i){
+       if(i>nums.size()-1)return 0;
+       if(i==nums.size()-1)return nums[i];
+       if(i==nums.size()-2)return max(nums[i],nums[i+1]);
 
-        for(int i=nums.size()-3;i>=0;i--){
-            dp[i]=max(dp[i+1],nums[i]+dp[i+2]);
-        }
-        return dp[0];
-        
+       if(dp[i]!=-1)return dp[i];
+
+       return dp[i]=max(nums[i]+tp(nums,i+2),tp(nums,i+1));
+
+
+    }
+  
+  
+  
+  
+    int rob(vector<int>& nums) {
+        dp.clear();
+        dp.resize(105,-1);
+         return tp(nums,0);
+    
+
+
+         
     }
 };
