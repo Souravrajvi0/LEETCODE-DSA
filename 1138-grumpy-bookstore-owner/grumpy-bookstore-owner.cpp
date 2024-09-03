87ad31c41628a15b0d533441d24244a7ac3052eb
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2024-09-04 00:49:29 +0530

    Time: 23 ms (67.19%) | Memory: 34.6 MB (52.76%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& c, vector<int>& g, int m) {
-       int n = c.size();
-        int prwdc = 0;
-        
-        // Calculate initial window satisfaction
-        for (int i = 0; i < m; ++i) {
-            if (g[i] == 1) {
-                prwdc += c[i];
-            }
+        int n=c.size();
+        int prwdc=0;
+        for(int i=0;i<m;i++){
+            if(g[i]==1) prwdc+=c[i];
         }
-        
-        int maxSatisfaction = prwdc;
-        int i = 0; // Start index of the window
-        int j = m; // End index of the window
-        
-        // Slide the window across the array
-        while (j < n) {
-            int currentSatisfaction = prwdc;
-            if (g[j] == 1) {
-                currentSatisfaction += c[j];
-            }
-            if (g[i] == 1) {
-                currentSatisfaction -= c[i];
-            }
-            
-            // Update max satisfaction and window indices
-            if (currentSatisfaction > maxSatisfaction) {
-                maxSatisfaction = currentSatisfaction;
-            }
-            
-            prwdc = currentSatisfaction;
-            ++i;
-            ++j;
-        }
-        
-        // Calculate satisfaction for all customers who are not greedy
-        int sumNonGreedy = 0;
-        for (int i = 0; i < n; ++i) {
-            if (g[i] == 0) {
-                sumNonGreedy += c[i];
-            }
-        }
-        
-        return maxSatisfaction + sumNonGreedy;
+        int max=prwdc;
+        int i=1;
+        int j=m;
+        int idx=0;
+        while(j<n){
+  
+       int crwdc=prwdc;
+       if(g[j]==1) crwdc+=c[j];
+       if(g[i-1]==1)crwdc-=c[i-1];
+       if(max<crwdc){
+        max=crwdc;
+        idx=i;
+       }
+       i++;
+       j++;
+       prwdc=crwdc;
+         }
+        int sum=0;
+       for(int i=0;i<n;i++){
+        if(g[i]==0)sum+=c[i];
+       }
+       return max+sum;
 
     }
 };
