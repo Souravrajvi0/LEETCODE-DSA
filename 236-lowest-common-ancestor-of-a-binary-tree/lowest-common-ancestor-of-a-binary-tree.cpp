7e2a977c5b7eca0b6dce2a0e9acfdef8fdb3d7e6
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2025-01-10 07:44:37 +0530

    Time: 10 ms (78.99%) | Memory: 18.9 MB (7.38%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,32 +9,38 @@
  */
 class Solution {
 public:
+     bool findP(TreeNode* root, TreeNode* node, vector<TreeNode*>&path){
+      if(!root) return false;
+      path.push_back(root);
+      if(root->val == node->val)return true;
+      
+      bool leftP = findP(root->left, node, path);
+      if(leftP) return true;
+
+      bool rightP = findP(root->right, node, path);
+      if(rightP) return true;
+      
+      path.pop_back();
+      return false;
+     }
+   
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-     if(root==NULL){
-         return root;
-     }
-     if(p->val==root->val||q->val==root->val){
-         return root;
-     }
-     TreeNode*temp1=lowestCommonAncestor(root->left,p,q);
-     TreeNode*temp2=lowestCommonAncestor(root->right,p,q);        
-     if(temp1==NULL&&temp2==NULL){
-         return NULL;
-     }
-     if(temp1==NULL&&temp2!=NULL){
-         return temp2;
-     }
-     if(temp1!=NULL&&temp2==NULL){
-         return temp1;
-     }
-     
-
-     return root;
-
-
-
-
-
+        vector<TreeNode*>p1;
+        findP(root,p,p1);
+        vector<TreeNode*>p2;
+        findP(root,q,p2);
+        int i = 0, j = 0;
+        TreeNode* lca = root;
+        while(i < p1.size() && j < p2.size()){
+         if(p1[i]->val == p2[j]->val){
+            lca= p1[i];
+         }else{
+            break;
+         }
+         i++;
+         j++;
+        }
+        return lca;
     }
 };
