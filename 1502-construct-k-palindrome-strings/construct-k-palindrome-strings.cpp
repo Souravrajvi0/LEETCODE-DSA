e57b450ef0dcf1b2730817756c7f7b2e5864fc44
Souravrajvi0 <144546710+Souravrajvi0@users.noreply.github.com>
2025-01-11 07:56:12 +0530

    Time: 30 ms (9.79%) | Memory: 15 MB (51.75%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if(s.size()<k) return false;
+        map<char,int>mp;
+        for(int i = 0 ;i <s.size() ;i++){
+            mp[s[i]]++;
+        }
+        int odd=0;
+        for(auto p : mp){
+          if(p.second % 2 != 0){
+            odd++;
+          }
+        }
+        if(odd>k) return false;
+        return true;
+        
+    }
+};
